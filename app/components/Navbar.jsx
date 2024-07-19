d18a9ec31b44f08bd472fbdf2603bc2d316a4841
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-07-19 20:45:43 +0400

    updating home

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,16 +1,23 @@
-import Link from 'next/link'
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import Logo from './dojo-logo.png'
 
 const Navbar = () => {
   return (
     <nav>
-    <h1>helpdesk</h1>
-    <ul className="menu">
-      <li><Link href="/">dashboard</Link></li>
-      <li><Link href="/tickets">tickets</Link></li>
-      <li><Link href=""></Link></li>
-    </ul>
-  </nav>
+      <Image
+      src={Logo}
+      alt="logo"
+      width={70}
+      quality={100}
+      placeholder='blur'
+      />
+      <h1>helpdesk</h1>
+      <Link href="/">dashboard</Link>
+      <Link href="/tickets">tickets</Link>
+      <Link href=""></Link>
+    </nav>
   )
 }
 
