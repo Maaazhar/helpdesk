f833344cc1e73d9cacbfb16098586f4d4ca34658
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-07-20 17:01:13 +0400

    implementing static generation on ticket compnent

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,3 +1,11 @@
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets/")
+  const tickets = await res.json()
+  return tickets.map((ticket) => ({
+    id: ticket.id
+  }))
+}
+
 async function getTicket(id) {
   const res = await fetch(`http://localhost:4000/tickets/${id}`, {
     next: {
@@ -10,7 +18,6 @@ async function getTicket(id) {
 
 
 export default async function TicketDetails({ params }) {
-  // const id = params.id
   const ticket = await getTicket(params.id)
 
   return (
