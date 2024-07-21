e4f19a0db5314071df6b05e149de9f9dec04ce35
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-07-21 23:36:40 +0400

    adding custom-loading with suspense-boundary to the ticket component

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link"
 
 async function getTickets() {
+  //imitate delay
+  await new Promise(resolve => setTimeout(resolve, 3000))
+
   const res = await fetch(`http://localhost:4000/tickets`, {
     next: {
       revalidate: 0 // using 0 for opt of cache use 
@@ -14,15 +17,15 @@ export default async function TicketList() {
   return (
     <>
       {tickets.map((ticket) => (
-          <Link href={`/tickets/${ticket.id}` } key={ticket.id}>
-        <div  className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
+        <Link href={`/tickets/${ticket.id}`} key={ticket.id}>
+          <div className="card my-5">
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
           </div>
-        </div>
-          </Link>
+        </Link>
       ))}
       {tickets.length === 0 && (
         <p className="text-center">There are no open tickets...</p>
